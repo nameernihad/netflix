b92215fbd1e8b3d0c41984628ed89d4afe941603
Nameer Nihad <nameernihadpc@gmail.com>
2023-06-02 22:01:51 +0530

    netflix banner,card,nav,static added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import './App.css';
 import NavBar from './Components/navBar/NavBar';
-
+import './App.css'
+import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Banner/>
+      <RowPost/>
     </div>
   );
 }
